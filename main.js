c3ddb7b5618c6f6fb4e17f6983a9fe327aa6bf6c
GMengZhi-Github <meng2772887108@outlook.com>
2021-04-04 13:27:31 +0800

    实例代码 方块检测及延迟 未实现

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 /* This main script should not included any function*/
+function interact(e){
+    var player = e.player;
+    var targetArchArray = getBlockesFromXYZ(e,0,0,0,1,1,1);
+    var playerArchArray = getBlockesFromXYZ(e,0,0,0,1,1,1);
+    if(targetArchArray === playerArchArray){
+        e.npc.scoreboard.setPlayerScore(player,"ArchLevel",1);
+    }
+}
